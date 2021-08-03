c6bca505266601ac05d221cbfd83011ce7948c91
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2021-08-03 09:34:29 -0400

    Eliminada validación de formulario Login

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -3,38 +3,39 @@
     <v-main>
       <v-container fluid fill-height>
         <v-layout flex align-center justify-center>
-          <v-flex  xs12 sm4 elevation-6>
-            <v-toolbar class="pt-5 blue darken-4">
-              <v-toolbar-items>
-                <v-toolbar-title class="white--text"><h4>LOGIN</h4></v-toolbar-title>
-              </v-toolbar-items>
-            </v-toolbar>
-            <v-card>
-              <v-card-text class="pt-4">
+          <v-flex xs12 sm3>
+            <v-card-actions class="justify-center display-1 mb-3">
+              <v-icon large class="mr-3">mdi-briefcase</v-icon>
+              LOGIN
+            </v-card-actions>
+            <v-card color="blue-grey lighten-5" elevation="6" class="pl-4 pr-4 pb-4">
+              <v-card-text>
                 <v-form v-model="valid" ref="form">
                   <v-text-field
                     label="User"
                     v-model="username"
-                    min="3"
-                    :rules="usernameRules"
-                    required
+                    prepend-icon="mdi-account"
                   ></v-text-field>
                   <v-text-field
                     label="Password"
                     v-model="password"
-                    min="4"
                     :append-icon="e1 ? 'mdi-eye' : 'mdi-eye-off'"
                     @click:append="() => (e1 = !e1)"
                     :type="e1 ? 'password' : 'text'"
-                    :rules="passwordRules"
-                    required
                   ></v-text-field>
-                  <v-layout align-end justify-end>
-                      <v-btn @click="submit" :class=" { 'blue darken-4 white--text' : valid, disabled: !valid }">Login</v-btn>
-                  </v-layout>
                 </v-form>
               </v-card-text>
-              </v-card>
+              <v-card-actions>
+                <v-btn
+                  block
+                  @click="submit"
+                  :class="{
+                    'blue darken-4 white--text': valid,
+                    disabled: !valid
+                  }"
+                >Login</v-btn>
+              </v-card-actions>
+            </v-card>
           </v-flex>
         </v-layout>
       </v-container>
@@ -49,13 +50,7 @@ export default {
       valid: false,
       e1: true,
       password: '',
-      passwordRules: [
-        (v) => !!v || 'Password is required',
-      ],
       username: '',
-      usernameRules: [
-        (v) => !!v || 'User is required',
-      ],
     }
   },
   methods: {
