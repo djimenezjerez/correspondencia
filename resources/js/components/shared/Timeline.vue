eb01c0463c2f066adf8712823d6cd2ef2d7ddd3a
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2021-08-21 08:33:17 -0400

    Añadido margen en puntos de seguimiento

--- resources/js/components/shared/Timeline.vue
+++ resources/js/components/shared/Timeline.vue
@@ -5,18 +5,27 @@
         <v-timeline-item
           v-for="(flow, index) in timeline"
           :key="index"
-          :class="{ 'text-right': index%2 && $vuetify.breakpoint.mdAndUp }"
+          class="text-center"
           :icon="index > 0 ? 'mdi-arrow-up-bold' : 'mdi-map-marker-radius'"
         >
-          <div class="font-weight-medium">
-            {{ flow.to_area }}
-          </div>
-          <div class="font-weight-regular">
-            {{ flow.action }}
-          </div>
-          <div class="font-weight-light">
-            {{ flow.date | moment('L LT') }}
-          </div>
+          <v-card
+            :dark="index == 0"
+            :color="index > 0 ? '' : '#666'"
+            class="rounded-card"
+            elevation="5"
+          >
+            <v-card-text>
+              <div class="font-weight-bold">
+                {{ flow.to_area }}
+              </div>
+              <div class="font-weight-medium">
+                {{ flow.action }}
+              </div>
+              <div class="font-weight-normal">
+                {{ flow.date | moment('L LT') }}
+              </div>
+            </v-card-text>
+          </v-card>
         </v-timeline-item>
       </v-col>
     </v-row>
@@ -34,3 +43,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.rounded-card{
+  border-radius: 15px;
+}
+</style>
