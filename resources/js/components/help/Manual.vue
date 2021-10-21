03fc279390739c6cb26aee9476299b1c55a84252
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2021-10-20 23:49:07 -0400

    Corregido localhost hardcoded

--- resources/js/components/help/Manual.vue
+++ resources/js/components/help/Manual.vue
@@ -2,8 +2,8 @@
   <v-container fluid>
     <v-row>
       <v-col cols="12">
-        <object style="width: 100%; height: 87vh;" data="http://localhost/assets/MANUAL.pdf" type="application/pdf">
-          <embed src="http://localhost/assets/MANUAL.pdf" type="application/pdf" />
+        <object style="width: 100%; height: 87vh;" data="/assets/MANUAL.pdf" type="application/pdf">
+          <embed src="/assets/MANUAL.pdf" type="application/pdf" />
         </object>
       </v-col>
     </v-row>
